fef619232ef8edf56bd615e46feb90a68bea92aa
k-Mitani0828 <core.editor>
2022-12-20 20:21:06 +0900

    カリキュラム8－５

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -22,6 +22,9 @@
                 <p class='body'>{{ $post->body }}</p>
              </div>
         </div>
+        <div class='edit'>
+            <a href="/posts/{{ $post->id }}/edit">edit</a></a>
+        </div>
         <div class='footer'>
             <a href="/">戻る</a>
         </div>
