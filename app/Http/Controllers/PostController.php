253bca520553edb3b6b62b007a0d1701203a4652
k-Mitani0828 <core.editor>
2022-12-21 16:40:04 +0900

    カリキュラム8－6

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -51,6 +51,12 @@ public function update(PostRequest $request,Post $post)
   $input_post = $request['post'];
   $post->fill($input_post)->save();
   return redirect('/posts/' .$post->id);
+  
+}
+public function delete(Post $post)
+{
+    $post->delete();
+    return redirect('/');
 }
 }
 ?>
